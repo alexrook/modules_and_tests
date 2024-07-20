2d541a58c51f835a6be988c17f8fa8c09d98d889
Александр Грачев <agrachev@neoflex.ru>
2024-07-20 13:58:49 +0000

    UnsignedCounter added

--- src/counter/unsigned.rs
+++ src/counter/unsigned.rs
@@ -1,11 +1,13 @@
-pub type UnsignedCounter = usize;
+pub struct UnsignedCounter(usize);
 
-pub fn default_unsigned_counter() -> UnsignedCounter {
-    0
-}
+impl UnsignedCounter {
+    pub fn new() -> Self {
+        UnsignedCounter(0)
+    }
 
-pub fn next_unsigned(counter: UnsignedCounter) -> UnsignedCounter {
-    counter + 1
+    pub fn next_unsigned(self) -> Self {
+        UnsignedCounter(self.0 + 1)
+    }
 }
 
 #[cfg(test)]
@@ -14,13 +16,13 @@ mod tests {
 
     #[test]
     fn default_unsigned_counter_should_return_0() {
-        let result: UnsignedCounter = default_unsigned_counter();
-        assert_eq!(result, 0);
+        let result: UnsignedCounter = UnsignedCounter::new();
+        assert_eq!(result.0, 0);
     }
 
     #[test]
     fn next_unsigned_should_return_1() {
-        let result: UnsignedCounter = next_unsigned(default_unsigned_counter());
-        assert_eq!(result, 1);
+        let result: UnsignedCounter = UnsignedCounter::new().next_unsigned();
+        assert_eq!(result.0, 1);
     }
 }
