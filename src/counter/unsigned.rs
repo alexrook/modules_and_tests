d3da253f2d1493f1140135c8290445851e0a2a89
Александр Грачев <agrachev@neoflex.ru>
2024-07-20 15:17:41 +0000

    UnsignedCounter new method renamed to default_unsigned due to clippy warnings

--- src/counter/unsigned.rs
+++ src/counter/unsigned.rs
@@ -1,7 +1,7 @@
 pub struct UnsignedCounter(usize);
 
 impl UnsignedCounter {
-    pub fn new() -> Self {
+    pub fn default_unsigned() -> Self {
         UnsignedCounter(0)
     }
 
@@ -16,13 +16,13 @@ mod tests {
 
     #[test]
     fn default_unsigned_counter_should_return_0() {
-        let result: UnsignedCounter = UnsignedCounter::new();
+        let result: UnsignedCounter = UnsignedCounter::default_unsigned();
         assert_eq!(result.0, 0);
     }
 
     #[test]
     fn next_unsigned_should_return_1() {
-        let result: UnsignedCounter = UnsignedCounter::new().next_unsigned();
+        let result: UnsignedCounter = UnsignedCounter::default_unsigned().next_unsigned();
         assert_eq!(result.0, 1);
     }
 }
